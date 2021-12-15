8ea73bb31caa26eec06a43c0f093a8777f626c3f
Francisco Neto <franmagneto@gmail.com>
2021-12-15 17:44:31 -0300

    Install telescope-ui-select.nvim

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -47,6 +47,7 @@ return require('packer').startup(function(use)
     requires = {'nvim-lua/plenary.nvim'},
   }
   use {'nvim-telescope/telescope-fzf-native.nvim', run="make"}
+  use 'nvim-telescope/telescope-ui-select.nvim'
 
   -- Git
   use 'airblade/vim-gitgutter' -- Show modified lines
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -197,6 +197,11 @@ _G.packer_plugins = {
     path = "/home/fneto/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
     url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
   },
+  ["telescope-ui-select.nvim"] = {
+    loaded = true,
+    path = "/home/fneto/.local/share/nvim/site/pack/packer/start/telescope-ui-select.nvim",
+    url = "https://github.com/nvim-telescope/telescope-ui-select.nvim"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/home/fneto/.local/share/nvim/site/pack/packer/start/telescope.nvim",
