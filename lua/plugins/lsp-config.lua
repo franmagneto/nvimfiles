f8800e75b9781108a386fc466e58c671cc93f232
Francisco Neto <franmagneto@gmail.com>
2021-12-16 10:54:19 -0300

    Fix info sign

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,8 +2,8 @@ vim.fn.sign_define('DiagnosticSignError',
   { text = '', texthl = 'DiagnosticSignError' })
 vim.fn.sign_define('DiagnosticSignWarn',
   { text = '', texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define('DiagnosticSignInformation',
-  { text = '', texthl = 'DiagnosticSignInformation' })
+vim.fn.sign_define('DiagnosticSignInfo',
+  { text = '', texthl = 'DiagnosticSignInfo' })
 vim.fn.sign_define('DiagnosticSignHint',
   { text = '', texthl = 'DiagnosticSignHint' })
 
