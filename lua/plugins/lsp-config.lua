8c5ca04bd620e293b4f343ab3f7f2f806ca77f53
Francisco Neto <franmagneto@gmail.com>
2021-12-16 10:47:41 -0300

    Override diagnostic signs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,3 +1,12 @@
+vim.fn.sign_define('DiagnosticSignError',
+  { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn',
+  { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInformation',
+  { text = '', texthl = 'DiagnosticSignInformation' })
+vim.fn.sign_define('DiagnosticSignHint',
+  { text = '', texthl = 'DiagnosticSignHint' })
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
