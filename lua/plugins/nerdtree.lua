988d771b6fda90af3cffc102a9e7137df4afdd9b
Francisco Neto <franmagneto@gmail.com>
2021-12-17 14:38:27 -0300

    Fix NERDTree behaviour with sessions

--- lua/plugins/nerdtree.lua
+++ lua/plugins/nerdtree.lua
@@ -1,13 +1,17 @@
 local g = vim.g
 local cmd = vim.cmd
-local fn = vim.fn
 
 g.NERDTreeFileExtensionHighlightFullName = 1
 g.NERDTreeExactMatchHighlightFullName = 1
 g.NERDTreePatternMatchHighlightFullName = 1
 
-if fn.filereadable(require'persistence'.get_current()) == 0 then
-  cmd [[autocmd VimEnter * NERDTree | wincmd p]]
-end
+cmd [[autocmd VimEnter * NERDTree | wincmd p]]
 
-cmd [[autocmd BufEnter * nested if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif]]
+-- Close Neovim if NERDTree is the last buffer
+cmd [[autocmd QuitPre * if winnr('$') == 2 && getbufvar(winbufnr(1), "&filetype") == "nerdtree" | NERDTreeClose | endif]]
+
+-- Prevent other buffers to open on NERDTree window
+cmd [[
+  autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
+    \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
+]]
